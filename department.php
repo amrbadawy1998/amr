3c2d1759119d5b93e8d31d5ab1f5a834e6a569fa
Amr badawy <62123887+amrbadawy1998@users.noreply.github.com>
2021-04-15 00:37:57 +0200

    Add files via upload

---
+++ department.php
@@ -0,0 +1,191 @@
+<?php
+include("header.php");
+?>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f1f1f1;
+  padding: 20px;
+  font-family: Arial;
+}
+
+/* Center website */
+.main {
+  max-width: 1000px;
+  margin: auto;
+}
+
+h1 {
+  font-size: 50px;
+  word-break: break-all;
+}
+
+.row {
+  margin: 8px -16px;
+}
+
+/* Add padding BETWEEN each column (if you want) */
+.row,
+.row > .column {
+  padding: 8px;
+}
+
+/* Create three equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 33.33%;
+  display: none; /* Hide columns by default */
+}
+
+/* Clear floats after rows */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Content */
+.content {
+  background-color: white;
+  padding: 10px;
+}
+
+/* The "show" class is added to the filtered elements */
+.show {
+  display: block;
+}
+
+/* Style the buttons */
+.btn {
+  border: none;
+  outline: none;
+  padding: 12px 16px;
+  background-color: white;
+  cursor: pointer;
+}
+
+/* Add a grey background color on mouse-over */
+.btn:hover {
+  background-color: #ddd;
+}
+
+/* Add a dark background color to the active button */
+.btn.active {
+  background-color: #666;
+   color: white;
+}
+</style>
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Our PORTFOLIO</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Gallery <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+<!-- MAIN (Center website) -->
+<div class="main">
+
+<h2>PORTFOLIO</h2>
+
+<div id="myBtnContainer">
+  <button class="btn active" onclick="filterSelection('all')"> Show all</button>
+  <button class="btn" onclick="filterSelection('photo')"> Photo</button>
+  <button class="btn" onclick="filterSelection('video')"> video</button>
+
+</div>
+
+<!-- Portfolio Gallery Grid -->
+<div class="row">
+  <div class="column photo">
+    <div class="content">
+      <img src="./DSCN2878.JPG" alt="Mountains" style="width:100%">
+    </div>
+  </div>
+  <div class="column photo">
+    <div class="content">
+    <img src="./DSCN2892.JPG" alt="Lights" style="width:100%">
+    </div>
+  </div>
+  <div class="column video">
+    <div class="content container">
+    <video width="320" height="240" autoplay muted>
+      <source src="./MVI_2722.MOV" type="video/mp4" style="width:50%">
+      <source src="./MVI_2722.MOV" type="video/ogg" style="width:50%">
+    Your browser does not support the video tag.
+    </video>
+
+    </div>
+  </div>
+  
+  <div class="column photo">
+    <div class="content">
+      <img src="./DSCN2892.JPG" alt="Car" style="width:100%">
+
+    </div>
+  </div>
+
+
+  </div>
+
+
+<!-- END GRID -->
+</div>
+
+<!-- END MAIN -->
+</div>
+
+<script>
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+</script>
+    <?php
+include("footer.php");
+?>
---
+++ doctor.php
@@ -0,0 +1,95 @@
+<?php
+include("header.php");
+?>
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Our Services</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Doctors <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+	  <section class="ftco-section ftco-services">
+    	<div class="container">
+        <div class="row">
+        	<div class="col-md-4 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block">
+              <div class="img w-100" style="background-image: url(images/dept-1.jpg);"></div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Service 1</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block">
+              <div class="img w-100" style="background-image: url(images/dept-2.jpg);"></div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Service 2</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-4 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block">
+              <div class="img w-100" style="background-image: url(images/dept-3.jpg);"></div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Service 3</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>      
+          </div>
+					
+					<div class="col-md-4 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block">
+              <div class="img w-100" style="background-image: url(images/dept-4.jpg);"></div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Service 4</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block">
+              <div class="img w-100" style="background-image: url(images/dept-5.jpg);"></div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Service 5</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-4 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block">
+              <div class="img w-100" style="background-image: url(images/dept-6.jpg);"></div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Service 6</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-4 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block">
+              <div class="img w-100" style="background-image: url(images/dept-7.jpg);"></div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Service 7</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-4 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block">
+              <div class="img w-100" style="background-image: url(images/dept-8.jpg);"></div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Service 8</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+              </div>
+            </div>      
+          </div>
+        </div>
+    	</div>
+    </section>
+		<?php
+		include("footer.php");
+		?>
